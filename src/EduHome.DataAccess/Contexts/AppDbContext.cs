95dc75526250333a0c7fb865df880604d7123430
Nuranakhmed <nuranakhmedova@gmail.com>
2023-01-17 01:45:55 -0800

    me

---
+++ src/EduHome.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHome.DataAccess.Contexts;
+
+public class AppDbContext:DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+	{
+
+	}
+	public DbSet<Course> Courses { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CourseConfigurations).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
